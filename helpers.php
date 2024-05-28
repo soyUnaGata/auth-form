c7ce2668e935de0cc87ba8faf592c50da5864a3d
soyUnaGata <fallen.snitch@gmail.com>
2024-05-28 13:33:06 +0300

    implement funcs

--- helpers.php
+++ helpers.php
@@ -5,6 +5,30 @@ function basePath($path = '')
     return __DIR__ . '/' . $path;
 }
 
+function loadView($name, $data = [])
+{
+    $viewPath = basePath("views/{$name}.view.php");
+
+    if (file_exists($viewPath)) {
+        extract($data);
+        require $viewPath;
+    } else {
+        echo "View '{$name} not found!'";
+    }
+}
+
+function loadPartial($name, $data = [])
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+
+    if (file_exists($partialPath)) {
+        extract($data);
+        require $partialPath;
+    } else {
+        echo "Partial '{$name} not found!'";
+    }
+}
+
 function inspectAndDie($value)
 {
     echo '<pre>';
