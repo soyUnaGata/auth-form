ed6af37a2a9c3a8daec01bbcb93f05210acb91ba
soyUnaGata <fallen.snitch@gmail.com>
2024-05-28 13:33:32 +0300

    implement views

--- public/index.php
+++ public/index.php
@@ -1,4 +1,19 @@
 <?php
 require '../helpers.php';
 
-require basePath('views/register.view.php');
+
+$routes = [
+    '/' => 'controllers/home.php',
+    '/register' => 'controllers/register.php',
+    '/login' => 'controllers/login.php',
+    '/logout' => 'controllers/logout.php',
+    '404' => 'controllers/error/404.php'
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+
+if (array_key_exists($uri, $routes)) {
+    require basePath($routes[$uri]);
+} else {
+    require basePath($routes['404']);
+}
---
+++ views/home.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
+    <title>Home</title>
+</head>
+<body>
+<main>
+    <section>
+        <a href="register.php">Register</a>
+        <span>or</span>
+        <a href="login.php">Login here</a>
+    </section>
+
+</main>
+</body>
+</html>
